40ebd47d316089ee02353f42ced49b77b32efd1c
Shane D Wilson <me@shanedwilson.com>
2018-09-18 21:15:17 -0500

    sytling

--- main.js
+++ main.js
@@ -10,21 +10,34 @@ const activateDeletes = () => {
     for (let i = 0; i < deleteButtons.length; i++) {
         const element = deleteButtons[i];
         element.addEventListener('click', (e) => {
-            console.log('Delete Clicked');
+            // card that button was on
+            const buttonClicked = e.target;
+            // .parentNode.remove
+            buttonClicked.parentNode.parentNode.remove();
         })
     }
-}
+};
+
+// document.querySelector('body').addEventListener('click', function(event) {
+//     if (event.target.classList.contains('deleteButton')) {
+//         element.addEventListener('click', (e) => {
+//             // card that button was on
+//             const buttonClicked = e.target;
+//             // .parentNode.remove
+//             buttonClicked.parentNode.parentNode.remove();        
+//     }
+// })
 
 const printToDom = (stringToPrint, whereToPrint) => {
     document.getElementById(whereToPrint).innerHTML += stringToPrint;
 };
 
 const buildNewToDoCard = (toDo, notes) => {
-    let domString = `<div class="card" style="width: 18rem;">
+    let domString = `<div class="card text-center w-25 m-2 mx-auto">
     <div class="card-body">
       <h5 class="card-title">${toDo}</h5>
       <p class="card-text">${notes}</p>
-      <button href="#" class="btn btn-primary deleteButton">Delete This Shit!</button>
+      <button href="#" class="btn btn-danger deleteButton">Delete This Shit!</button>
     </div>
   </div>`;
 
