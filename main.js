71579d24e7651d3b3ede9d771755b84e94d17e38
Shane D Wilson <me@shanedwilson.com>
2018-09-18 20:01:38 -0500

    deletes added

--- main.js
+++ main.js
@@ -3,21 +3,34 @@ const notesInputElem = document.getElementById('notesInput');
 
 const submitToDoButtonElem = document.getElementById('submitToDoButton');
 
+
+const activateDeletes = () => {
+    const deleteButtons = document.getElementsByClassName('deleteButton');
+
+    for (let i = 0; i < deleteButtons.length; i++) {
+        const element = deleteButtons[i];
+        element.addEventListener('click', (e) => {
+            console.log('Delete Clicked');
+        })
+    }
+}
+
 const printToDom = (stringToPrint, whereToPrint) => {
     document.getElementById(whereToPrint).innerHTML += stringToPrint;
-}
+};
 
 const buildNewToDoCard = (toDo, notes) => {
     let domString = `<div class="card" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title">${toDo}</h5>
       <p class="card-text">${notes}</p>
-      <a href="#" class="btn btn-primary">Delete</a>
+      <button href="#" class="btn btn-primary deleteButton">Delete This Shit!</button>
     </div>
   </div>`;
 
     printToDom(domString, 'toDoCards');
-}
+    activateDeletes();
+};
 
 submitToDoButtonElem.addEventListener('click', (e) => {
     e.preventDefault();
